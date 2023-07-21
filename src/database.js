cd298378c7d948c2199ba0b345ace36df3bd9bc5
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2023-07-21 16:29:18 +0300

    feat: database.js

---
+++ src/database.js
@@ -0,0 +1,39 @@
+import pgPromise from 'pg-promise';
+import dotenv from 'dotenv';
+
+const pgp = pgPromise();
+dotenv.config();
+
+// Connection à la base de donnée:
+const db = pgp({
+    host: 'localhost',
+    port: 5432,
+    database: 'akory',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+  });
+
+// Test de connection:
+db.connect()
+  .then(obj => {
+    console.log('Connecté à la base de données avec succès.');
+    obj.done(); // Libère le pool de clients si vous n'avez plus besoin de la connexion.
+  })
+  .catch(error => {
+    console.error('Erreur de connexion à la base de données:', error);
+  });
+
+/**
+ * 
+ * NOTE:
+ * J'ai mis l' user et le mots de passe de
+ * psql dans un variable d'environnement dondenv
+ * cacher sous gitignire
+ * donc, avant de tester la connection à la base de
+ * donnée: crées d'abord un fichier app .env
+ * et mettez-y les lignes suivants:
+ * 
+ * DB_USER=postgres <-- à changer
+ * DB_PASSWORD=password <-- à changer
+ * 
+ * */
