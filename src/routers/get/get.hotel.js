877d21c43a24d2e693f32f8e462be898f3312fc1
Daris02 <hei.raymond.2@gmail.com>
2023-07-24 10:45:22 +0300

    fix: update function get

--- src/routers/get/get.hotel.js
+++ src/routers/get/get.hotel.js
@@ -1,10 +1,12 @@
-import { router } from "../router";
+import { router } from "../router.js";
+import { handlePromise } from "../promiseHandler.js";
+import { getReceptionistsListInWhichHotel } from "../../DAO/displays.js";
 
 // EASY-LINES:
 // ------------------------------------------------------------------------------------------------------------------------
 // easy-line2:
 // DISPLAY THE LIST OF RECEPTIONISTS WITH THE HOTEL TO WHICH THEY ARE ATTACHED 
-router.get("/ReceptionistsListInWhichHotel", (req, res) => {
+export const getHotel = (req, res) => {
     // FOR TEST: http://localhost:8000/ReceptionistsListInWhichHotel
     handlePromise(getReceptionistsListInWhichHotel(), res);
-});
+  }
