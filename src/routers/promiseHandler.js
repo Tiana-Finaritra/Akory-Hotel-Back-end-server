a61a78a4bad6088fd61ef596b6979ef6c4db98ae
Daris02 <hei.raymond.2@gmail.com>
2023-07-24 09:10:01 +0300

    feat: new promise for insertion

--- src/routers/promiseHandler.js
+++ src/routers/promiseHandler.js
@@ -18,3 +18,12 @@ export function handlePromise(promise, res) {
         res.status(500).send("Server Error");
       });
 }
+
+export const handlePromiseInsertion = async (promise, res) => {
+  try {
+    const result = await promise;
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({error: error.message});
+  }
+}
