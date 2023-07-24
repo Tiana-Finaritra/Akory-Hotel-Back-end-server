9456f654d9aa200c3668bd0c5ae4775f6a282898
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2023-07-24 09:49:53 +0300

    docs: "isolate getByEntries"

--- src/routers/router.js
+++ src/routers/router.js
@@ -4,7 +4,8 @@ import {
   getAllRoomsByTypeAndHotelName
   , getAnaliseBeneficPromotion
   , getAverageResNumberDaysByHotel
-  , getAverageResNumberMonthsByHotelAndYear, getBookingNumberByCustomer
+  , getAverageResNumberMonthsByHotelAndYear
+  , getBookingNumberByCustomer
   , getCollectedPayForAllHotelsByYear
   , getCurrentPrommotionsList
   , getCurrentlyOccupiedRoomsList
@@ -42,12 +43,6 @@ export const router = express.Router();
 
 // EASY-LINES:
 // ------------------------------------------------------------------------------------------------------------------------
-// easy-line2:
-// DISPLAY THE LIST OF RECEPTIONISTS WITH THE HOTEL TO WHICH THEY ARE ATTACHED 
-router.get("/ReceptionistsListInWhichHotel", (req, res) => {
-  // FOR TEST: http://localhost:8000/ReceptionistsListInWhichHotel
-  handlePromise(getReceptionistsListInWhichHotel(), res);
-})
 
 // easy-line3:
 // DISPLAY THE LIST OF RESERVATIONS STARTING WITH THE MOST RECENT FOR A GIVEN HOTEL
