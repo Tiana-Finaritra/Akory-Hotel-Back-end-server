bb2595d025569101da5f8a81c91f5f8a8545b8ef
Daris02 <hei.raymond.2@gmail.com>
2023-07-24 21:58:09 +0300

    fix: update function update province

--- src/routers/update/putById.js
+++ src/routers/update/putById.js
@@ -1,15 +1,42 @@
 import { handlePromiseUpdate } from "../promiseHandler.js";
-import queries from "../../Queries/allUpdate/allUpdate.js";
+import queriesUp from "../../Queries/allUpdate/allUpdate.js";
+import queriesOne from "../../Queries/OneSelect/allOneSelect.js";
 import { generalUpdate } from "../../DAO/update.js";
 import getElemtFunction from "../get/getById.js";
+import { pool } from "../../database.js";
 
 let updateFunction;
 
 const updateProvinceAvailable = (req, res) => {
-    const oldElem = getElemtFunction.getProvinceAvailableById;
-    console.log(oldElem);
-    res.end();
-    // handlePromiseUpdate(generalUpdate(queries.updateProvinceAvailable, []), res);
+    // const new_name = req.query.province_name;
+    // const new_code = req.query.code_province;
+    const new_name =  req.body.province_name;
+    const new_code = req.body.code_province;
+
+    pool.query(queriesOne.getProvinceAvailableById, [req.params.id], (error, result) => {
+        const oldElem = result.rows;
+        if(!oldElem.length){
+            res.send("Province not exits");
+        } else {
+            let id = req.params.id
+            let final_name = oldElem[0].province_name;
+            let final_code = oldElem[0].code_province;
+
+            if (new_name){
+                final_name = new_name;
+            }
+            if (new_code){
+                final_code = new_code;
+            }
+
+            console.log(
+                id,
+                final_name,
+                final_code
+            );
+            handlePromiseUpdate(generalUpdate(queriesUp.updateProvinceAvailable, [id, final_name, final_code]), res);
+        }
+    });
 }
 
 export default updateFunction = {
