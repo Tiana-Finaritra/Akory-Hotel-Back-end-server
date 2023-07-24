b1d4a7b2b10353c7323261971d6a6b1333286964
Daris02 <hei.raymond.2@gmail.com>
2023-07-24 15:23:59 +0300

    feat: add queries

---
+++ src/Queries/allSelect/allQueries.js
@@ -0,0 +1,19 @@
+import ReceptionistsListInWhichHotelQ from "./1-easy/easy-line2.js";
+import ReservationListDescByHotelQ from "./1-easy/easy-line3.js";
+import AllRoomsByTypeAndHotelNameQ from "./1-easy/easy-line4.js";
+import OffersBySeasonAnDHotelQ from "./1-easy/easy-line5.js";
+import ReservationOfGivenCustomerQ from "./1-easy/easy-line6.js";
+import CustomersListNotPaidFullFeesQ from "./1-easy/easy-line7.js";
+import TotalPayReceidByGivenMethodQ from "./1-easy/easy-line8.js";
+
+let queries;
+
+export default queries = {
+  ReceptionistsListInWhichHotelQ,
+  ReservationListDescByHotelQ,
+  AllRoomsByTypeAndHotelNameQ,
+  OffersBySeasonAnDHotelQ,
+  ReservationOfGivenCustomerQ,
+  CustomersListNotPaidFullFeesQ,
+  TotalPayReceidByGivenMethodQ,
+};
