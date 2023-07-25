c72cd4b0176da14236e2b7674091b8200f10ac64
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2023-07-26 00:20:32 +0300

    style: "refromate: promiseHanler"

--- src/DAO/delete.js
+++ src/DAO/delete.js
@@ -2,7 +2,7 @@ import { db } from "../database.js";
 
 export const generalDelete = (query, params) => {
     try {
-        return db.query(query, params);
+        return db.result(query, params);
     } catch {
         console.log(err);
         throw new Error(err.message);
