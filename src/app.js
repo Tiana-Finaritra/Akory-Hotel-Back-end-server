957cea1aa0e8a14c780c7e232986b306fac9b762
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2023-07-27 06:44:07 +0300

    feat: "token an auth

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { router } from "./routers/router.js";
-import { AuthRouter } from "./security/autentificate.js";
+import { AuthRouter, verifyToken } from "./security/autentificate.js";
 
 const app = express();
 
@@ -17,6 +17,17 @@ app.use(router);
 app.get("/", (req, res) => {
   res.send("go live to index.html");
 });
+
+// acces on some ressources:
+app.get("/resource", verifyToken, (req, res) => {
+  /**
+   * Ici, nous pouvons autoriser l'accès à la ressource car le token est valide.
+   *  L'utilisateur authentifié est disponible dans req.user 
+   * (par exemple, req.user.email).
+  */
+  res.json({ message: "Ressource protégée accessible" });
+});
+
 
 // launch server
 app.listen(8000, () => {
--- src/security/autentificate.js
+++ src/security/autentificate.js
@@ -1,5 +1,9 @@
 import express, { Router } from "express";
 import { db } from "../database.js";
+import jwt from "jsonwebtoken";
+
+const jwtSecretKey = "fcshfvkhlshvhlshbvbhsbvgsdiuvhulivhsuih";
+
 
 export const AuthRouter = express.Router();
 
@@ -8,12 +12,26 @@ AuthRouter.post("/login",async (req, res) => {
     try {
         const user = await db.oneOrNone(`SELECT cu.name, cu.email, cu.password FROM "customer" cu WHERE cu.email = $1;`, [user_email]);
         if (user && user.password === password) {
-            res.json({ message: 'Authentification réussie' });
-        }else {
+            const token = jwt.sign({ email: user.email }, jwtSecretKey, { expiresIn: "1h" });
+            res.json({ message: 'Authentification réussie',"user_name": user.name, token: token });
+          } else {
             res.status(401).json({ error: 'Identifiants invalides' });
-        }
+          }
     } catch (error) {
         console.log(error.message);
         res.status(500).json({ error: 'Une erreur est survenue' });
     }
 });
+
+ export function verifyToken(req, res, next) {
+    const token = req.header("Authorization");
+    if (!token) return res.status(401).json({ error: 'Accès non autorisé' });
+  
+    jwt.verify(token, jwtSecretKey, (err, decoded) => {
+      if (err) return res.status(401).json({ error: 'Token invalide' });
+  
+      req.user = decoded;
+  
+      next();
+    });
+  }
