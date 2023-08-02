5c3b0a2f8872c84d5f69f6e374223195b820fb1b
Daris02 <hei.raymond.2@gmail.com>
2023-08-02 12:09:30 +0300

    fix: update parameter of payment method

--- src/Queries/allSelect/1-easy/easy-line8.js
+++ src/Queries/allSelect/1-easy/easy-line8.js
@@ -4,9 +4,9 @@ const TotalPayReceidByGivenMethodQ = `
     SELECT p.id, p.payment_date, p.amount_paid, p.number_night, p.room_occuped,
         p.deadline_payment, p.lending_status, p.total_amount_status, c.name, c.last_name,
         CONCAT_WS(',',
-            CASE WHEN pm.mobile_money = true THEN 'mobile_money' ELSE NULL END,
-            CASE WHEN pm.credit_card = true THEN 'credit_card' ELSE NULL END,
-            CASE WHEN pm.cash = true THEN 'cash' ELSE NULL END
+            CASE WHEN pm.name = 'mobile_money' THEN 'mobile_money' ELSE NULL END,
+            CASE WHEN pm.name = 'credit_card' THEN 'credit_card' ELSE NULL END,
+            CASE WHEN pm.name = 'cash' THEN 'cash' ELSE NULL END
         ) AS paid_by
     FROM payment p 
     INNER JOIN customer c ON c.id = p.id_customer
