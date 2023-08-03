e98350ede90e9c782d7939baa4e6fc6ce7db67c9
Daris02 <hei.raymond.2@gmail.com>
2023-08-03 12:30:13 +0300

    feat: add new function special

---
+++ src/Queries/allSpecial/reservationsCustomer.js
@@ -0,0 +1,23 @@
+const ResrvationsWithCustomerInfo = `
+    SELECT 
+        re.id,
+        re.date_arrived,
+        re.leaving_date,
+        re.number_of_person,
+        re.is_cancelled,
+        cu.name AS customer_firstname,
+        cu.last_name AS customer_lastname,
+        cu.principal_contact,
+        cu.emergency_number,
+        cu.address,
+        cu.email,
+        cu.cin,
+        cu.gender,
+        ro.room_type
+    FROM reservation re
+        INNER JOIN customer cu ON re.id_customer = cu.id
+        INNER JOIN room ro ON re.id_room = ro.id
+    ;
+`;
+
+export default ResrvationsWithCustomerInfo;
